cf437f9f239cdd8003f2f3b7f22bcc943f05682e
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2023-12-26 14:30:50 -0300

    Create dataset.tf

---
+++ dataset.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<name>.json")
+
+  project = "cloud-treino"
+  region  = "southamerica-east1"
+  zone    = "southamerica-east1-b"
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
